7902b26ff364ac9380d38ed40ea32e4114b6a7dc
Ujjaini Das <ujjaini@cs.utexas.edu>
2022-12-04 19:59:40 -0600

    add twist messages to proportionally adjust angular yaw with error from line

--- calculate_error.py
+++ calculate_error.py
@@ -1,12 +1,12 @@
 import rospy
 from std_msgs.msg import String
-import tty, sys, termios
 
 if __name__ == '__main__':
     ros_pub = rospy.Publisher('node_camera', String, queue_size=10)
     rospy.init_node('node_camera_pub')
     rate = rospy.Rate(60)
-    x = 0
+    x = -0.1
     while not rospy.is_shutdown():
-        ros_pub.publish(x)
+        ros_pub.publish(str(x))
         rate.sleep()
+
--- follow_line.py
+++ follow_line.py
@@ -7,20 +7,20 @@ from sensor_msgs.msg import PointCloud2
 class itl_run:
 
     def __init__(self) -> None:
-        self.error = 0 
+        self.error = 0
         self.msg = Twist()
-        self.msg.angular.x = 0.0 # forward and back
+        self.msg.angular.x = 0.0 # yaw
         self.msg.angular.y = 0.0
         self.msg.angular.z = 0.0
         self.msg.linear.x = 0.0
         self.msg.linear.y = 0.0
-        self.msg.linear.z = 0.0 # yaw
+        self.msg.linear.z = 0.0 # forward and back
         # self.pid = PID(1, 0.1, 0.05, setpoint=1)
 
     def callback(self, data):
         self.error = data.data
         print(self.error)
-        rospy.loginfo("The error is %s", self.error)
+        rospy.loginfo("The error is %f", float(self.error))
 
     def run_prog(self):
         ros_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
@@ -30,7 +30,11 @@ class itl_run:
         while not rospy.is_shutdown():
             print(self.error)
             self.msg.linear.x = 0.3
-            
+
+            self.msg.angular.z = float(self.error)
+
+            print("running robot")
+
             ros_pub.publish(self.msg)
             rate.sleep()
 if __name__ == '__main__':
