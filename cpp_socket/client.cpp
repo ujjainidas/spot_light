2de8565c670df434b2ca26e741cc423bbe018bb5
Ujjaini Das <thisisujjaini@gmail.com>
2022-12-06 16:56:10 -0600

    add socket in cpp and python

---
+++ cpp_socket/client.cpp
@@ -0,0 +1,51 @@
+// Client side C/C++ program to demonstrate Socket
+// programming
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <iostream>
+
+#define PORT 8080
+
+using namespace std;
+
+int main(int argc, char const* argv[])
+{
+	int sock = 0, valread, client_fd;
+	struct sockaddr_in serv_addr;
+    float val = 3.14;
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	// Convert IPv4 and IPv6 addresses from text to binary
+	// form
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
+		<= 0) {
+		printf(
+			"\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
+	if ((client_fd
+		= connect(sock, (struct sockaddr*)&serv_addr,
+				sizeof(serv_addr)))
+		< 0) {
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	send(sock, &val, sizeof(float), 0);
+	cout << val << endl;
+	valread = read(sock,&val, sizeof(float));
+	cout << val << endl;
+
+	// closing the connected socket
+	close(client_fd);
+	return 0;
+}
--- follow_line.py
+++ follow_line.py
@@ -1,13 +1,19 @@
 import rospy
+import socket
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import PointCloud2
-# from simple_pid import PID
+# from simple_pid import PID		
 
 class itl_run:
 
     def __init__(self) -> None:
         self.error = 0
+
+        self.port = 8080	
+        self.s = socket.socket()
+        s.connect(('127.0.0.1', port))	
+
         self.msg = Twist()
         self.msg.angular.x = 0.0 # yaw
         self.msg.angular.y = 0.0
@@ -17,19 +23,21 @@ class itl_run:
         self.msg.linear.z = 0.0 # forward and back
         # self.pid = PID(1, 0.1, 0.05, setpoint=1)
 
-    def callback(self, data):
-        self.error = data.data
-        print(self.error)
-        rospy.loginfo("The error is %f", float(self.error))
+    # def callback(self, data):
+    #     self.error = data.data
+    #     print(self.error)
+    #     rospy.loginfo("The error is %f", float(self.error))
 
     def run_prog(self):
         ros_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-        rospy.init_node('node_camera_sub', anonymous=True)
-        ros_key = rospy.Subscriber('node_camera', String, self.callback)
+        # rospy.init_node('node_camera_sub', anonymous=True)
+        # ros_key = rospy.Subscriber('node_camera', String, self.callback)
         rate = rospy.Rate(60)
         while not rospy.is_shutdown():
             print(self.error)
             self.msg.linear.x = 0.3
+
+            self.error = s.recv(1024).decode()
 
             self.msg.angular.z = float(self.error)
 
@@ -37,6 +45,8 @@ class itl_run:
 
             ros_pub.publish(self.msg)
             rate.sleep()
+        self.s.close()
+
 if __name__ == '__main__':
     app_prog = itl_run()
     app_prog.run_prog()
